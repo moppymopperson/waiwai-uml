9cd80ceb2da0177a6a9044d24aeab188e2a1dadf
Erik Hornberger <hornbergererik@gmail.com>
2018-04-13 23:11:13 -0400

    Added room label and updated comments

--- src/App.js
+++ src/App.js
@@ -15,14 +15,33 @@ import yText from '../node_modules/y-text/dist/y-text.js'
 import yMap from '../node_modules/y-map/dist/y-map.js'
 Y.extend(yMap, yArray, yText, yWebsocketsClient, yMemory)
 
+/**
+ * Defines a single page app with an editing area for
+ * markdown and a rendering area that shows the PlantUML
+ * output.
+ */
 class App extends Component {
+
+  /**
+   * State holds the latest version of the users' encoded
+   * PlantUML markdown as well as some user preference data
+   */
   state = { encodedUml: '', vimMode: false }
 
+  /**
+   * This will be triggered as soon as the app loads. Here we
+   * connect to the room and debounce the `handleTextUpdate` method.
+   */
   componentDidMount() {
     this.handleTextUpdate = debounce(this.handleTextUpdate, 1000)
     this.joinRoom()
   }
 
+  /**
+   * The string defined here is appended to the top of every PlantUML
+   * request and defines the colors and styling of the image that will be
+   * returned.
+   */
   plantUmlStyling() {
     return `
     skinparam class {
@@ -33,6 +52,15 @@ class App extends Component {
     `
   }
 
+  /**
+   * Connects to the websocket server responsible for propogating updates
+   * across users and sets up a single shared variable.
+   * 
+   * We use an in memory database for speed and simplicity. 
+   * Users will be put in a room based on the URL they are at. Users in the
+   * same room will be able to see and edit the same data, but users in
+   * different rooms will be able to work independently.
+   */
   joinRoom() {
     console.log('Joining...')
     Y({
@@ -64,12 +92,28 @@ class App extends Component {
       })
   }
 
+  /**
+   * Called each time the markdown text changes. In this method we encode the
+   * PlantUML markdown that can be sent to a PlantUML server for processing. We
+   * then update the state wit the latest value, which will be used by an
+   * <img> tag to fetch an SVG image from the server.
+   * 
+   * Note that this method gets debounced in `componentDidMount`. Not doing so
+   * results in a heavy load on the server that can cause it to crash with just
+   * one client.
+   * 
+   * @param {string} text PlantUML markdown
+   */
   handleTextUpdate(text) {
     const encodedUml = plantumlEncoder.encode(this.plantUmlStyling() + text)
     this.setState({ encodedUml })
     console.log(`http://${window.location.hostname}:8080/svg/${encodedUml}`)
   }
 
+  /**
+   * The options specified here will be applied to the CodeMirror text editor
+   * the first time it mounts.
+   */
   codeOptions() {
     return {
       mode: 'markdown',
@@ -93,13 +137,16 @@ class App extends Component {
           }}>VIM Mode: {this.state.vimMode ? 'ON' : 'OFF'}</button>
         </nav>
         <div className="container">
-          <ReactCodeMirror
-            value={this.state.code}
-            options={this.codeOptions()}
-            editorDidMount={editor => {
-              this.editor = editor
-            }}
-          />
+          <div className="vert">
+            Room: {window.location.pathname.slice(1)}
+            <ReactCodeMirror
+              value={this.state.code}
+              options={this.codeOptions()}
+              editorDidMount={editor => {
+                this.editor = editor
+              }}
+            />
+          </div>
           <div className="uml">
             <img
               src={`http://${window.location.hostname}:8080/svg/${
@@ -109,8 +156,16 @@ class App extends Component {
             />
           </div>
         </div>
-        <div className="footer"><div><a href="http://plantuml.com/class-diagram" target="_blank" rel="noopener noreferrer">Markdown Cheat Sheet</a></div>
-          Copyright Erik Hornberger, EExT LLC, 2018</div>
+        <div className="footer">
+          <div>
+            <a href="http://plantuml.com/class-diagram"
+              target="_blank"
+              rel="noopener noreferrer">
+              Markdown Cheat Sheet
+            </a>
+          </div>
+          Copyright Erik Hornberger, EExT LLC, 2018
+        </div>
       </div>
     )
   }
